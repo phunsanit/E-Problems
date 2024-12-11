00a0fff38b103af6df2b04f2037025b78dd98261
pitt phunsanit <5268121+phunsanit@users.noreply.github.com>
2024-12-11 18:11:41 +0700

    init

---
+++ SourceCode/resources/js/app.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+import ExampleComponent from './components/ExampleComponent.vue';
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        ExampleComponent,
+    },
+});
