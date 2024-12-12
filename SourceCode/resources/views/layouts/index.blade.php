5eba4434daef2619be4d3f3d3901c8d7204d7135
pitt phunsanit <5268121+phunsanit@users.noreply.github.com>
2024-12-13 02:51:46 +0700

    add Tailwind CSS

---
+++ SourceCode/resources/views/layouts/index.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Tailwind Project</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js']) 
+</head>
+<body>
+    @yield('content') 
+</body>
+</html>
--- SourceCode/tailwind.config.js
+++ SourceCode/tailwind.config.js
@@ -1,11 +1,16 @@
 module.exports = {
-    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
     darkMode: false, // or 'media' or 'class'
+    plugins: [],
+    purge: ['./resources/**/*.blade.php', './resources/**/*.js', './resources/**/*.vue'],
     theme: {
         extend: {},
     },
     variants: {
         extend: {},
     },
-    plugins: [],
 }
