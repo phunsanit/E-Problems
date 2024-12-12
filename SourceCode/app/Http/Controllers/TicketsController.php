c86a8537af5c601d43ebb63ef53db566951e6c84
pitt phunsanit <5268121+phunsanit@users.noreply.github.com>
2024-12-13 00:45:13 +0700

    backup

--- SourceCode/app/Http/Controllers/TicketsController.php
+++ SourceCode/app/Http/Controllers/TicketsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
 
-class TicketController extends Controller
+class TicketsController extends Controller
 {
     /**
      * Display a listing of the resource.
--- SourceCode/routes/web.php
+++ SourceCode/routes/web.php
@@ -1,10 +1,41 @@
 <?php
 
+use App\Http\Controllers\TicketsController;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//tickets frontend
-Route::resource('tickets', TicketController::class);
+//refresh cache
+Route::get('/refresh', function () {
+    //clear
+    Artisan::call('cache:clear');
+    Artisan::call('clear-compiled');
+    Artisan::call('config:clear');
+    Artisan::call('optimize:clear');
+    Artisan::call('route:clear');
+    Artisan::call('schedule:clear-cache');
+    Artisan::call('view:clear');
+
+    //add
+    Artisan::call('cache:table');
+    Artisan::call('cache:cache');
+    Artisan::call('config:cache');
+    Artisan::call('event:cache');
+    Artisan::call('lang:publish');
+    Artisan::call('optimize');
+    Artisan::call('route:cache');
+    Artisan::call('view:cache');
+
+    return "All caches have been cleared and recreated.";
+});
+
+Route::controller(TicketsController::class)->group(function () {
+    Route::delete('/tickets/{id}', 'destroy');
+    Route::get('/tickets', 'index');
+    Route::get('/tickets/{id}', 'show');
+    Route::post('/tickets', 'store');
+    Route::put('/tickets/{id}', 'update');
+});
