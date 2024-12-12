2d421b797455a0b052c72a19335afbd0944b9335
pitt phunsanit <5268121+phunsanit@users.noreply.github.com>
2024-12-12 17:22:00 +0700

    migration relational table
    move relational from seed to migration ( i think it true location )

--- SourceCode/database/seeders/DatabaseSeeder.php
+++ SourceCode/database/seeders/DatabaseSeeder.php
@@ -3,10 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Schema;
 
 class DatabaseSeeder extends Seeder
 {
@@ -33,77 +30,14 @@ class DatabaseSeeder extends Seeder
             CategoriesSeeder::class,
             OrganizationsSeeder::class,
             ServiceLinesSeeder::class,
-            ServiceLinesVesselsSeeder::class,
             SupportEngineerSeeder::class,
             VesselsSeeder::class,
+
+            // add after ServiceLines and Vessels
+            ServiceLinesVesselsSeeder::class,
 
             //add at last
             TicketsSeeder::class,
         ]);
-
-        //add Relational Constraints
-        Schema::table('tickets', function (Blueprint $table) {
-            $table->foreign('category_id', 'fk_tickets_categories')
-                ->references('id')
-                ->on('categories');
-
-            $table->foreign('support_engineer_id', 'fk_tickets_support_engineer')
-                ->references('user_id')
-                ->on('support_engineer');
-
-            $table->foreign('vessel_id', 'fk_tickets_vessels')
-                ->references('id')
-                ->on('vessels');
-        });
-
-        Schema::table('vessels', function (Blueprint $table) {
-            $table->foreign('organization_id', 'fk_vessels_organizations')
-                ->references('id')
-                ->on('organizations');
-        });
-
-        Schema::table('service_lines_vessels', function (Blueprint $table) {
-            $table->foreign('service_line_id', 'fk_service_lines')
-                ->references('id')
-                ->on('service_lines');
-
-            $table->foreign('vessel_id', 'fk_vessels')
-                ->references('id')
-                ->on('vessels');
-
-            $table->unique(['vessel_id', 'service_line_id'], 'service_lines_vessels_vessel_id_service_line_id_unique');
-        });
-
-        Schema::table('support_engineer', function (Blueprint $table) {
-            $table->foreign('user_id', 'fk_support_engineer_users')
-                ->references('id')
-                ->on('users');
-        });
-
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
-    {
-        Schema::table('tickets', function (Blueprint $table) {
-            $table->dropForeign('fk_tickets_categories');
-            $table->dropForeign('fk_tickets_support_engineer');
-            $table->dropForeign('fk_tickets_vessels');
-        });
-
-        Schema::table('vessels', function (Blueprint $table) {
-            $table->dropForeign('fk_vessels_organizations');
-        });
-
-        Schema::table('service_lines_vessels', function (Blueprint $table) {
-            $table->dropForeign('fk_service_lines');
-            $table->dropForeign('fk_vessels');
-        });
-
-        Schema::table('support_engineer', function (Blueprint $table) {
-            $table->dropForeign('fk_support_engineer_users');
-        });
     }
 }
