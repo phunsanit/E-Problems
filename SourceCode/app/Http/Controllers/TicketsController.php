09eb305b1759a7fab38503e01216a55d340de7bb
pitt phunsanit <5268121+phunsanit@users.noreply.github.com>
2024-12-14 10:58:43 +0700

    add validation

--- SourceCode/app/Http/Controllers/TicketsController.php
+++ SourceCode/app/Http/Controllers/TicketsController.php
@@ -26,23 +26,23 @@ class TicketsController extends Controller
 
         if ($request->has('search') && !empty($request->search)) {
 
-            if ($request->search['id']) {
-                $query->where('id', $request->search['id']);
+            if (!empty($request->search['id']) && is_int((int) $request->search['id'])) {
+                $query->where('id', (int) $request->search['id']);
             }
 
-            if ($request->search['category_id']) {
+            if ($request->search['category_id'] && is_int((int) $request->search['category_id'])) {
                 $query->where('category_id', $request->search['category_id']);
             }
 
-            if ($request->search['service_lines_id']) {
+            if ($request->search['service_lines_id'] && is_int((int) $request->search['service_lines_id'])) {
                 $query->where('service_lines_id', $request->search['service_lines_id']);
             }
 
-            if ($request->search['status_id']) {
+            if ($request->search['status_id'] && is_int((int) $request->search['status_id'])) {
                 $query->where('status_id', $request->search['status_id']);
             }
 
-            if ($request->search['support_engineer_id']) {
+            if ($request->search['support_engineer_id'] && is_int((int) $request->search['support_engineer_id'])) {
                 $query->where('support_engineer_id', $request->search['support_engineer_id']);
             }
 
