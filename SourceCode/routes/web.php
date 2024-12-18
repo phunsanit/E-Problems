666fbe41e9b17ce3f9d613747fa144f5df953997
Pitt Phunsanit <Phunsanit@gmail.com>
2024-12-19 00:35:00 +0700

    fixed duplicate route

--- SourceCode/routes/web.php
+++ SourceCode/routes/web.php
@@ -25,8 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
+//require __DIR__.'/auth.php';
 
 //refresh cache
 Route::get('/refresh', function () {
