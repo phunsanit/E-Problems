7ca33a695d91c392a7d21680a500ee683797ec13
Pitt Phunsanit <Phunsanit@gmail.com>
2024-12-19 00:14:59 +0700

    merge web soute

--- SourceCode/routes/web.php
+++ SourceCode/routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TicketsController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -25,3 +27,58 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//refresh cache
+Route::get('/refresh', function () {
+    //php artisan optimize:clear
+    $commands = [
+
+        //clear
+        'auth:clear-resets',
+        'optimize:clear',
+        'queue:clear',
+        'schedule:clear-cache',
+
+        //add
+        'cache:table',
+        'lang:publish',
+        'optimize',
+        'stub:publish',
+        //'ziggy:generate',
+
+        //infomation
+        'about',
+        //'db:show',
+        'env',
+        'list', //all commands
+        'package:discover',
+        //'route:list',//all routes but must be in console only
+    ];
+
+    $output = '<!DOCTYPE html><html lang="en"><head><title>refresh</title></head><body><dl>';
+
+    foreach ($commands as $command) {
+        if (Artisan::call($command) === 0) {
+            $results = Artisan::output();
+            $results = explode("\n", $results);
+            $results = implode("</dd><dd>", $results);
+            $output .= '<dt>php artisan ' . $command . '</dt><dd>' . $results . '</dd>';
+        } else {
+            $output .= '<dt>php artisan ' . $command . '</dt><dd><h2>Error executing command</h2></dd>';
+        }
+    }
+
+    $output .= '</dl><br>All caches have been cleared and recreated.</body></html>';
+
+    return response($output)->header('Content-Type', 'text/html');
+});
+
+Route::controller(TicketsController::class)->group(function () {
+    Route::delete('/tickets/{id}', 'destroy');
+    Route::get('/tickets', 'index');
+    Route::get('/tickets/{id}', 'show');
+    Route::get('/tickets/{id}/edit', 'edit');
+    Route::get('/tickets/create', 'create');
+    Route::post('/tickets', 'store');
+    Route::put('/tickets/{id}', 'update');
+});
