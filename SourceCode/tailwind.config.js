d34477eda4bcce0ef5d86b369a453c3733a778f1
Pitt Phunsanit <Phunsanit@gmail.com>
2024-12-25 02:01:53 +0700

    fixed build warn

--- SourceCode/tailwind.config.js
+++ SourceCode/tailwind.config.js
@@ -6,11 +6,9 @@ import typography from '@tailwindcss/typography';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './resources/js/**/*.vue', 
-        './resources/views/**/*.blade.php', 
-        './storage/framework/views/*.php',
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        "./resources/**/*.js", // Keep this to include any JavaScript files with Tailwind directives
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
     theme: {
         extend: {
--- SourceCode/vite.config.js
+++ SourceCode/vite.config.js
@@ -3,6 +3,18 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.name.endsWith('.woff2') || assetInfo.name.endsWith('.ttf')) {
+                        return 'fonts/[name][extname]';
+                    }
+                    return 'assets/[name]-[hash][extname]';
+                },
+            },
+        },
+    },
     css: {
         preprocessorOptions: {
             scss: {
@@ -29,6 +41,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
+            '@': '/resources/js',
             vue: 'vue/dist/vue.esm-bundler.js',
         },
     },
