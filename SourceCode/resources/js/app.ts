afbbe3dd136531edf0f3b6543c4a0bd06bac522c
Pitt Phunsanit <Phunsanit@gmail.com>
2025-01-03 20:37:07 +0700

    backup call vue.js from view

--- SourceCode/resources/js/app.ts
+++ SourceCode/resources/js/app.ts
@@ -34,25 +34,16 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
+import DatetimeText from './Components/DatetimeText.vue';
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
-        resolvePageComponent(
-            `./Pages/${name}.vue`,//SPA pages
-            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
-        ),
-    setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
-    },
-    progress: {
-        color: '#4B5563',
+const app = createApp({
+    components: {
+        DatetimeText,
     },
 });
+app.mount('#app'); 
 
 // global variables
 declare global {
