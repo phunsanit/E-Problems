1739631f8041be3dd3053c6bae5c54f26c90313b
Pitt Phunsanit <Phunsanit@gmail.com>
2025-01-04 17:44:49 +0700

    no message

--- SourceCode/resources/js/app.ts
+++ SourceCode/resources/js/app.ts
@@ -46,4 +46,38 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import './jquery';
 
 //Vue.js
-import './vue';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
+import { ZiggyVue } from 'ziggy-js';
+
+// import component
+import LocaleDate from './Components/LocaleDate.vue';
+import LocaleTime from './Components/LocaleTime.vue';
+
+let el = document.getElementById('app');
+
+let app; // Declare app variable
+
+if (el) { //inertiajs
+    createInertiaApp({
+        resolve: name => import(`./Pages/${name}`),
+        setup({ el, App, props, plugin }) {
+            app = createApp({ render: () => h(App, props) })
+
+                .use(plugin)
+                .use(ZiggyVue)
+        },
+    });
+} else {
+    el = document.getElementById('appModule');
+    app = createApp({})// different from createInertiaApp
+}
+
+//common components for all pages
+if (app && el) {
+    app
+        .component('LocaleDate', LocaleDate)
+        .component('LocaleTime', LocaleTime)
+
+        .mount(el);
+}
